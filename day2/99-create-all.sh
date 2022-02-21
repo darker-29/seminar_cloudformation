a6d701ea4c7e12c320efc4fc1be6c3dce7325290
darker-29 <darker29.developer@gmail.com>
2022-02-21 22:38:39 +0900

    fix: 指定ファイル名の修正

--- day2/99-create-all.sh
+++ day2/99-create-all.sh
@@ -11,8 +11,8 @@ bash 05-route53.sh ${ARGMENT}
 bash 06-acm.sh ${ARGMENT}
 bash 10-network.sh ${ARGMENT}
 bash 20-ec2.sh ${ARGMENT}
-bash 25-alb.sh ${ARGMENT}
-bash 26-autoscaling.sh ${ARGMENT}
+bash 25-autoscaling.sh ${ARGMENT}
+bash 26-alb.sh ${ARGMENT}
 bash 30-rds.sh ${ARGMENT}
 
 exit 0
