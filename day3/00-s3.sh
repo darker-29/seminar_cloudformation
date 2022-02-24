56b6d37e5e4e1709a1474ef75f74b2a77aacb97e
darker-29 <darker29.developer@gmail.com>
2022-02-24 23:44:30 +0900

    add: s3デプロイ用のshellスクリプト作成

---
+++ day3/00-s3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source $PWD/.env
+
+CHANGESET_OPTION="--no-execute-changeset"
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+  echo "deploy mode"
+  CHANGESET_OPTION=""
+fi
+
+
+CFN_STACK_NAME=${ENV}-s3
+CFN_TEMPLATE=$PWD/s3.yml
+
+aws cloudformation deploy \
+    --stack-name ${CFN_STACK_NAME} \
+    --template-file ${CFN_TEMPLATE} \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides \
+    Env=${ENV} \
+    S3BucketName=${S3_BUCKET_NAME}
+
+
+exit 0
