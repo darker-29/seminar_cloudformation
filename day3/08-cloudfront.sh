b152e093dec898d1bc1c40e3df66525e853819e8
darker-29 <darker29.developer@gmail.com>
2022-02-24 23:57:24 +0900

    add: cloudfrontデプロイ用のShellスクリプト

---
+++ day3/08-cloudfront.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source $PWD/.env
+
+CHANGESET_OPTION="--no-execute-changeset"
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+  echo "deploy mode"
+  CHANGESET_OPTION=""
+fi
+
+
+CFN_STACK_NAME=${ENV}-cloufront
+CFN_TEMPLATE=$PWD/cloudfront.yml
+
+aws cloudformation deploy \
+    --stack-name ${CFN_STACK_NAME} \
+    --template-file ${CFN_TEMPLATE} \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides \
+    Env=${ENV} \
+    Domain=${DOMAIN}
+
+
+exit 0
