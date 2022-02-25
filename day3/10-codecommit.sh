28e6bbd516c8bceef5631e0f96b3798ee5d2f54b
darker-29 <darker29.developer@gmail.com>
2022-02-25 20:59:38 +0900

    add: codecommit, codepipeline用のシェルスクリプト整備

---
+++ day3/10-codecommit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source $PWD/.env
+
+CHANGESET_OPTION="--no-execute-changeset"
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+  echo "deploy mode"
+  CHANGESET_OPTION=""
+fi
+
+
+CFN_STACK_NAME=${ENV}-codecommit
+CFN_TEMPLATE=$PWD/codecommit.yml
+
+aws cloudformation deploy \
+    --stack-name ${CFN_STACK_NAME} \
+    --template-file ${CFN_TEMPLATE} \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides \
+    Env=${ENV}
+
+
+exit 0
---
+++ day3/15-codepipeline.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source $PWD/.env
+
+CHANGESET_OPTION="--no-execute-changeset"
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+  echo "deploy mode"
+  CHANGESET_OPTION=""
+fi
+
+
+CFN_STACK_NAME=${ENV}-codepipeline
+CFN_TEMPLATE=$PWD/codepipeline.yml
+
+aws cloudformation deploy \
+    --stack-name ${CFN_STACK_NAME} \
+    --template-file ${CFN_TEMPLATE} \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides \
+    Env=${ENV}
+
+
+exit 0
